e3bb8c50016993fc4da92b7bf870550a912e0024
Eugenia Galantseva <lucyatsky@gmail.com>
2021-01-26 23:49:47 -0600

    lesson 1

---
+++ variablePlay.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
+
+function variablePlay() {
+    let p = document.getElementById('result');
+    let admin = name = 'Василий';
+
+    p.innerHTML = admin;
+};
